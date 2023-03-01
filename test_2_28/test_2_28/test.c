e1e0cf03d9e5adc1e034ffabc5d9b520e2a3e42f
First-encounter <1651204505@qq.com>
2023-02-28 21:46:51 -0700

    test_2_28

---
+++ test_2_28/test_2_28/test.c
@@ -0,0 +1,69 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	a = a + b;
+//	b = a - b;
+//	a = a - b;
+//	printf("a=%d,b=%d\n", a, b);
+//	return 0;
+//}
+//
+//
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	int tmp = 0;
+//	tmp = a;
+//	a = b;
+//	b = tmp;
+//	printf("a=%d,b=%d\n", a, b);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("12345\n67890");
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//#define PI 3.14
+//int main()
+//{
+//	int r1 = 0;
+//	int r2 = 0;
+//	double s1 = 0;
+//	double s2 = 0;
+//	printf("请输入大圆的半径r1，小圆的半径r2(r1>r2)：>");
+//	scanf("%d%d", &r1, &r2);
+//	s1 = PI * r1 * r1 / 2.0;
+//	s2 = PI * r2 * r2 / 2.0;
+//	printf("大圆的面积=%lf\n小圆的面积=%lf\n大圆的面积减小圆的面积=%lf\n", s1, s2, s1 - s2);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#define PI 3.14nnn
+//int main()
+//{
+//	int r1 = 25;
+//	int r2 = 15;
+//	double s1 = 0;
+//	double s2 = 0;
+//	s1 = PI * r1 * r1 / 2.0;
+//	s2 = PI * r2 * r2 / 2.0;
+//	printf("大圆的面积=%lf\n小圆的面积=%lf\n大圆的面积减小圆的面积=%lf\n", s1, s2, s1 - s2);
+//	return 0;
+//}
